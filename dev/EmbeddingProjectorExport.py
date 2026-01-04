32571a66444514374752d5ad6ff984df88ad1727
William Landers <wlanders770@gmail.com>
2026-01-04 16:19:33 -0600

    updating vector file

--- dev/EmbeddingProjectorExport.py
+++ dev/EmbeddingProjectorExport.py
@@ -45,7 +45,6 @@ def export_for_projector():
         # 4. Export Vectors TSV
         with open('vectors.tsv', 'w', encoding='utf-8', newline='') as f:
             writer = csv.writer(f, delimiter='\t')
-            writer.writerow(['Vector_Data'])
             for vector in embeddings:
                 writer.writerow(vector)
         
